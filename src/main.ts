56bf2020eeab0da024b82df48a4c2777360881e0
Belayet Hossain <2020belayethossain@gmail.com>
2023-11-21 00:20:17 +0600

    chore: app running log update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log('App Run on Port 3000');
+  Logger.log('App Run on Port 3000');
 }
 bootstrap();
