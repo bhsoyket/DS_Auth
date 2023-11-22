6cc0f789d7a8b277f9b049f553a14bb58bf6f2e4
ruhulcse <ruhul.cse7862@gmail.com>
2023-11-23 01:12:44 +0600

    basic database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+require('dotenv').config();
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host:process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: false
+    },
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
