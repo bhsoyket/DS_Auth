f2ab8489bb0d83839174bf5379e1e5c2b90075c2
Belayet Hossain <2020belayethossain@gmail.com>
2023-11-25 23:25:31 +0600

    chore: DB  and ENV config refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-require('dotenv').config();
+import { envConfig } from 'config/env.config';
+import { dbConfig } from 'config/db.config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host:process.env.DB_HOST,
-      port: +process.env.DB_PORT,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-      entities: [],
-      synchronize: true,
-      ssl: {
-        rejectUnauthorized: false
-    },
-    }),
+    ConfigModule.forRoot(envConfig),
+    TypeOrmModule.forRoot(dbConfig),
 
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,11 +1,16 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
 require('dotenv').config();
-export const dbConfig = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    entities: [],
-    synchronize: true,
-  }
+
+export const dbConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [],
+  synchronize: true,
+  ssl: {
+    rejectUnauthorized: false
+  },
+}
